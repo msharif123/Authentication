97f6b36e4d84a0c66f30a953e80466ba74e04183
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-01-14 23:33:56 +0100

    -add som files and folders

--- server.js
+++ server.js
@@ -3,33 +3,60 @@ const express= require ("express");
 const bcrypt = require ("bcrypt")
 const app = express();
 
-app.use( express.json())
+app.use(express.json())
 const PORT= process.env.PORT || 4001
 
-
+let users=[]
 
 app.get ("/users", (req, res)=>{
-    res.send("Hello Jensen")
+    res.json("Hello Jensen")
 })
 
 
 
 app.post("/users",async (req, res)=>{
-    
-    try { const salt= await bcrypt.genSalt()
-        const hashedPassword= await bcrypt.hash(req.body.password,salt) 
-        console.log(salt)
-        console.log(hashedPassword)
+   
+    try { 
+        // const salt= await bcrypt.genSalt()
+        const hashedPassword= await bcrypt.hash(req.body.password,10) 
+        // console.log(salt)
+        // console.log(hashedPassword)
         const user = {"name": req.body.name, password: hashedPassword}
+
+        users.push(user);
+
         res.status (200).json({Message: "users created successfully", user })
     } catch  (error) {
-        res.status(500).send("somethin is wrong with  server")
-        
+        res.status(500).send("somethin is wrong with this server")
+     
+        res.send(req.hashedPassword)
     }
-   
-    res.send(req.hashedPassword)
     
+   
 })
+
+
+    app.post("/users/login", async (req,res)=>{
+
+        const user= users.find(user => user.name === req.body.name)
+        if (user==null){
+            return res.status(400).send("cannot find the user")        }
+        
+        try {
+            if(await bcrypt.compare(req.body.password, user.password)){
+                res.send ("user login successfully")
+                
+
+            }else{
+                res.send("not allowed")
+            }
+        } catch (error) {
+            res.status(500).send( )
+            
+        }
+
+    
+ })
 
 
 
@@ -39,3 +66,8 @@ app.post("/users",async (req, res)=>{
 
 
 app.listen (PORT, console.log ( `This server is listening on port ${PORT}`))
+
+
+
+
+
