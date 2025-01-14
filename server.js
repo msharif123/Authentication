4f9bc793c3f26d35a8d69d50418bdc42024ff3c1
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-01-14 21:08:24 +0100

    initial

---
+++ server.js
@@ -0,0 +1,41 @@
+const express= require ("express");
+
+const bcrypt = require ("bcrypt")
+const app = express();
+
+app.use( express.json())
+const PORT= process.env.PORT || 4001
+
+
+
+app.get ("/users", (req, res)=>{
+    res.send("Hello Jensen")
+})
+
+
+
+app.post("/users",async (req, res)=>{
+    
+    try { const salt= await bcrypt.genSalt()
+        const hashedPassword= await bcrypt.hash(req.body.password,salt) 
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = {"name": req.body.name, password: hashedPassword}
+        res.status (200).json({Message: "users created successfully", user })
+    } catch  (error) {
+        res.status(500).send("somethin is wrong with server")
+        
+    }
+   
+    res.send(req.hashedPassword)
+    
+})
+
+
+
+
+
+
+
+
+app.listen (PORT, console.log ( `This server is listening on port ${PORT}`))
